c2e90bbffd38ef8a63c82f039347ab59f7f4d9a7
Nicholas Niles <nicholasniles0@gmail.com>
2026-03-21 18:51:59 -0400

    Anim system basic

---
+++ Scripts/AnimationObject.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "newAnimation", menuName = "New Animation Object")]
+public class AnimationObject : ScriptableObject
+{
+    public Sprite[] animFrames;
+}
