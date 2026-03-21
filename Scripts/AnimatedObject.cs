368e34584eb05a023ef593ac144ce00f80028e79
Nicholas Niles <nicholasniles0@gmail.com>
2026-03-21 18:59:17 -0400

    Anim Idle

--- Scripts/AnimatedObject.cs
+++ Scripts/AnimatedObject.cs
@@ -2,15 +2,41 @@ using UnityEngine;
 
 public class AnimatedObject : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    public SpriteRenderer render;
+    public AnimationObject[] animations;
+    public float timePerFrame = 0.5f;
+
+    [Header("View Only")]
+    public int currentAnimationIndex = -1;
+    public int currentFrame = 0;
+    public float frameTimer;
+
+    public void PlayAnim(int index)
     {
-        
+        currentAnimationIndex = index;
+        currentFrame = 0;
+        frameTimer = 0;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        frameTimer += Time.deltaTime;
+        if (currentAnimationIndex != -1)
+        {
+            //Playing an anim
+            if (frameTimer > timePerFrame)
+            {
+                frameTimer = 0;
+                currentFrame++;
+                if (currentFrame >= animations[currentAnimationIndex].animFrames.Length)
+                {
+                    currentFrame = 0;
+                }
+
+                render.sprite = animations[currentAnimationIndex].animFrames[currentFrame];
+            }
+        }
     }
+
 }
