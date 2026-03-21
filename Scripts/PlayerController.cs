ad5f4a9b9fbf8c09448e8615f45406d1ac71a9cd
Nicholas Niles <nicholasniles0@gmail.com>
2026-03-21 13:12:00 -0400

    Stamina Added

--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -1,5 +1,6 @@
 using System;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerController : MonoBehaviour
 {
@@ -10,12 +11,21 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float jumpForce = 25f;
     [SerializeField] private float attackCooldown = 1.0f;
     [SerializeField] private float attackActionCooldown = 0.25f;
+    [SerializeField] private float staminaRechargeCooldown = 0.5f;
+    [SerializeField] private float staminaRegenerationRate = 0.1f;
+    [SerializeField] private float maxStamina = 100f;
+    [SerializeField] private float staminaJump = 20f;
+    [SerializeField] private float staminaPunch = 15f;
+    [SerializeField] private float staminaAirPunch = 30f;
+    [SerializeField] private float staminaKick = 15f;
+    [SerializeField] private float staminaAirKick = 30f;
 
     [Header("Player Objects")]
     [SerializeField] private CollisionTrigger groundDetection;
     [SerializeField] private Rigidbody2D playerPhysics;
     [SerializeField] private GameObject[] attackOrigin;
     [SerializeField] private GameObject[] attackPrefabs;
+    [SerializeField] private Image staminaBarFill;
 
     [Header("For Viewing")]
     [SerializeField] private bool isGrounded = false;
@@ -28,6 +38,8 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private KeyCode lastInput;
     [SerializeField] private float timeSinceLastInput = 0.0f;
     [SerializeField] private float timeSinceAttack = 0.0f;
+    [SerializeField] private float timeSinceStaminaUse = 0.0f;
+    [SerializeField] private float stamina = 0;
 
     [SerializeField] private GameObject currentAttackObject;
 
@@ -40,6 +52,11 @@ public class PlayerController : MonoBehaviour
     };
 
     [SerializeField] private dirData dir;
+
+    private void Start()
+    {
+        stamina = maxStamina;
+    }
 
     private void FixedUpdate()
     {
@@ -54,8 +71,26 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        staminaBarFill.fillAmount = stamina / maxStamina;
+
         timeSinceAttack += Time.deltaTime;
         timeSinceLastInput += Time.deltaTime;
+        timeSinceStaminaUse+= Time.deltaTime;
+
+        if (timeSinceStaminaUse >= staminaRechargeCooldown)
+        {
+            stamina += staminaRegenerationRate;
+
+            if (stamina > maxStamina)
+            {
+                stamina = maxStamina;
+            }
+        }
+
+        if (stamina < 0)
+        {
+            stamina = 0;
+        }
 
         if (lastInput == KeyCode.D)
         {
@@ -84,34 +119,50 @@ public class PlayerController : MonoBehaviour
 
             if (timeSinceAttack > attackCooldown && Input.GetKey(KeyCode.Mouse0) || timeSinceAttack > attackCooldown && Input.GetKey(KeyCode.Mouse1))
             {
-                timeSinceAttack = 0;
-
-                if (isGrounded)
-                {
-                    movementVel.x = 0;
-                }
 
                 if (Input.GetKey(KeyCode.Mouse0))
                 {
-                    lastInput = KeyCode.Mouse0;
                     if (isGrounded)
                     {
-                        print("Punching...");
-                        if (dir == dirData.Right)
+                        if (stamina >= staminaPunch)
                         {
-                            currentAttackObject = Instantiate(attackPrefabs[0], attackOrigin[0].transform.position, attackOrigin[0].transform.rotation, attackOrigin[0].transform);
-                        }
-                        else if (dir == dirData.Left)
-                        {
-                            currentAttackObject = Instantiate(attackPrefabs[0], attackOrigin[1].transform.position, attackOrigin[1].transform.rotation, attackOrigin[1].transform);
+                            //PUNCH triggered here!
+                            stamina -= staminaPunch;
+                            print("Punching...");
+                            lastInput = KeyCode.Mouse0;
+                            timeSinceAttack = 0;
+                            timeSinceStaminaUse = 0;
+
+                            if (isGrounded)
+                            {
+                                movementVel.x = 0;
+                            }
+                            if (dir == dirData.Right)
+                            {
+                                currentAttackObject = Instantiate(attackPrefabs[0], attackOrigin[0].transform.position, attackOrigin[0].transform.rotation, attackOrigin[0].transform);
+                            }
+                            else if (dir == dirData.Left)
+                            {
+                                currentAttackObject = Instantiate(attackPrefabs[0], attackOrigin[1].transform.position, attackOrigin[1].transform.rotation, attackOrigin[1].transform);
+                            }
                         }
                     }
                     else
                     {
-                        if (!hasUsedAirAttack)
+                        if (!hasUsedAirAttack && stamina >= staminaAirPunch)
                         {
+                            //AIR PUNCH triggered here!
+                            stamina -= staminaAirPunch;
                             hasUsedAirAttack = true;
                             print("Air Punching...");
+                            lastInput = KeyCode.Mouse0;
+                            timeSinceAttack = 0;
+                            timeSinceStaminaUse = 0;
+
+                            if (isGrounded)
+                            {
+                                movementVel.x = 0;
+                            }
                             if (dir == dirData.Right)
                             {
                                 currentAttackObject = Instantiate(attackPrefabs[2], attackOrigin[0].transform.position, attackOrigin[0].transform.rotation, attackOrigin[0].transform);
@@ -126,25 +177,47 @@ public class PlayerController : MonoBehaviour
                 //Punch takes priority over kick if both inputs used simultaneously
                 else if (Input.GetKey(KeyCode.Mouse1))
                 {
-                    lastInput = KeyCode.Mouse1;
                     if (isGrounded)
                     {
-                        print("Kicking...");
-                        if (dir == dirData.Right)
+                        if (stamina >= staminaAirKick)
                         {
-                            currentAttackObject = Instantiate(attackPrefabs[1], attackOrigin[2].transform.position, attackOrigin[2].transform.rotation, attackOrigin[2].transform);
-                        }
-                        else if (dir == dirData.Left)
-                        {
-                            currentAttackObject = Instantiate(attackPrefabs[1], attackOrigin[3].transform.position, attackOrigin[3].transform.rotation, attackOrigin[3].transform);
+                            //KICK triggered here!
+                            stamina -= staminaKick;
+                            print("Kicking...");
+                            lastInput = KeyCode.Mouse1;
+                            timeSinceAttack = 0;
+                            timeSinceStaminaUse = 0;
+
+                            if (isGrounded)
+                            {
+                                movementVel.x = 0;
+                            }
+                            if (dir == dirData.Right)
+                            {
+                                currentAttackObject = Instantiate(attackPrefabs[1], attackOrigin[2].transform.position, attackOrigin[2].transform.rotation, attackOrigin[2].transform);
+                            }
+                            else if (dir == dirData.Left)
+                            {
+                                currentAttackObject = Instantiate(attackPrefabs[1], attackOrigin[3].transform.position, attackOrigin[3].transform.rotation, attackOrigin[3].transform);
+                            }
                         }
                     }
                     else
                     {
-                        if (!hasUsedAirAttack)
+                        if (!hasUsedAirAttack && stamina >= staminaAirKick)
                         {
+                            //AIR KICK triggered here!
+                            stamina -= staminaAirKick;
                             hasUsedAirAttack = true;
                             print("Air Kicking...");
+                            lastInput = KeyCode.Mouse1;
+                            timeSinceAttack = 0;
+                            timeSinceStaminaUse = 0;
+
+                            if (isGrounded)
+                            {
+                                movementVel.x = 0;
+                            }
                             if (dir == dirData.Right)
                             {
                                 currentAttackObject = Instantiate(attackPrefabs[3], attackOrigin[2].transform.position, attackOrigin[2].transform.rotation, attackOrigin[2].transform);
@@ -182,11 +255,14 @@ public class PlayerController : MonoBehaviour
             }
 
 
-            if (Input.GetKey(KeyCode.Space) && isGrounded)
+            if (Input.GetKey(KeyCode.Space) && isGrounded && stamina >= staminaJump)
             {
+                //JUMP triggered here!
+                stamina -= staminaJump;
                 movementVel.y = jumpForce;
                 lastInput = KeyCode.Space;
                 timeSinceLastInput = 0;
+                timeSinceStaminaUse = 0;
             }
 
             if (!Input.GetKey(KeyCode.Space) && isGrounded)
