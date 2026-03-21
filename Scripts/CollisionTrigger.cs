f89471219223ebd27962a9aab219f38256e35d64
Nicholas Niles <nicholasniles0@gmail.com>
2026-03-20 21:09:26 -0400

    Movement

--- Scripts/CollisionTrigger.cs
+++ Scripts/CollisionTrigger.cs
@@ -1,16 +1,41 @@
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class CollisionTrigger : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    public List<GameObject> currentlyTouching = new List<GameObject>();
+    public UnityEvent onCollisionEnter;
+    public UnityEvent onCollisionExit;
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        
+        if (!currentlyTouching.Contains(collision.gameObject))
+        {
+            currentlyTouching.Add(collision.gameObject);
+            onCollisionEnter.Invoke();
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerStay2D(Collider2D collision)
     {
-        
+        if (!currentlyTouching.Contains(collision.gameObject))
+        {
+            currentlyTouching.Add(collision.gameObject);
+            onCollisionEnter.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (currentlyTouching.Contains(collision.gameObject))
+        {
+            currentlyTouching.Remove(collision.gameObject);
+            onCollisionExit.Invoke();
+        }
     }
 }
--- Scripts/PlayerController.cs
+++ Scripts/PlayerController.cs
@@ -3,16 +3,75 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    [SerializeField] private bool isGrounded = false;
+    [SerializeField] private CollisionTrigger groundDetection;
+    [SerializeField] private Rigidbody2D playerPhysics;
 
+    [SerializeField] private Vector2 movementVel;
+    [SerializeField] private Vector2 environmentVel;
+    [SerializeField] private Vector2 naturalVel;
+    [SerializeField] private Vector2 velocity;
 
-    void Start()
+    [SerializeField] private float gravityStrength = -9.6f;
+    [SerializeField] private float movementSpeed = 5f;
+    [SerializeField] private float jumpForce = 25f;
+
+    private KeyCode lastInput;
+    private float timeSinceLastInput = 0.0f;
+
+    private void FixedUpdate()
     {
-        
+        if (!isGrounded)
+        {
+            movementVel.y += gravityStrength;
+        }
+
+        velocity = movementVel + environmentVel + naturalVel;
+        playerPhysics.linearVelocity = velocity;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetKey(KeyCode.D) && isGrounded)
+        {
+            movementVel.x = movementSpeed;
+            lastInput = KeyCode.D;
+        }
+        if (Input.GetKey(KeyCode.A) && isGrounded)
+        {
+            movementVel.x = -movementSpeed;
+            lastInput = KeyCode.A;
+        }
+
+        if (Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A) && isGrounded || !Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A) && isGrounded)
+        {
+            movementVel.x = 0.0f;
+        }
+
+
+        if (Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            movementVel.y = jumpForce;
+            lastInput = KeyCode.Space;
+        }
+
+        if (!Input.GetKey(KeyCode.Space) && isGrounded)
+        {
+            movementVel.y = 0;
+        }
+    }
+
+    public void GroundCollider()
+    {
+        for (int i = 0; i < groundDetection.currentlyTouching.Count; i++)
+        {
+            if (groundDetection.currentlyTouching[i].tag == "Ground")
+            {
+                isGrounded = true;
+                return;
+            }
+        }
+
+        isGrounded = false;
     }
 }
